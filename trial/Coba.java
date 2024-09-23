1dc360337602ff507e6d2a8ae0faa32def47fe85
MarsaZaidan <122515807+MeeDdan@users.noreply.github.com>
2024-09-23 08:13:32 +0700

    Add files via upload

---
+++ trial/Coba.java
@@ -0,0 +1,8 @@
+
+public class Coba{
+    public static void main(String[] args){
+        System.out.println("Nama : M Zaidan Zidna Fann");
+        System.out.println("Nim : 607062400042");
+        System.out.println("Kelas : D3IF-48-01");
+    }
+}
